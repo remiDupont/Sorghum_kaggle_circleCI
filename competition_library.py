85b4218d4107c050affc92b8442667c3082700db
remiDupont <remidupont991@gmail.com>
2022-12-10 15:40:44 +0100

    added :  randomly_subsample

--- competition_library.py
+++ competition_library.py
@@ -1,2 +1,30 @@
-def label_func(path_obj):
+from fastai.vision.all import *
+
+import random
+
+
+
+def label_func_0(path_obj):
     return 1
+
+
+
+
+def randomly_subsample(files,prob_keep:float):
+    
+    """function that will return a subsequence of infut list (file)
+    Used to train and validate on a subpart of the dataset 
+
+    Args:
+        files (_type_): input list to pick elements from
+        float (prob_keep): proportion of files to keep
+
+    Returns:
+        _type_: sub_sequence of files 
+    """
+
+    split_file = []
+    for f in files:
+        if random.random()< prob_keep:
+            split_file.append(f)
+    return split_file
--- test_competition_library.py
+++ test_competition_library.py
@@ -1,4 +1,7 @@
 from competition_library import *
 
 def test_label_func():
-    assert label_func(1) == 1
+    assert label_func_0(1) == 1
+
+def fonction_sans_t():
+    assert 1 == 1 
